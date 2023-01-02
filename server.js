6a0a321817ea27f5b45fcdbf11ec06b632d85612
Suraj Jadhavar <jsuraj61@gmail.com>
2023-01-02 11:24:06 +0530

    Port no changes

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/',(req,resp)=>{
     resp.sendFile('public/form.html',{root:__dirname})
 })
 
-app.listen(2000,()=>{
+app.listen(7000,()=>{
     console.log("server started at 2k port")
 })
